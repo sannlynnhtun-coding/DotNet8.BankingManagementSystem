e4413ba309c5bab749b1bcc295b46f8c64971a03
Sann Lynn Htun <sannlynnhtun.developer@gmail.com>
2024-02-28 03:18:06 +0630

    -

--- DotNet8.BankingManagementSystem.BackendApi/Program.cs
+++ DotNet8.BankingManagementSystem.BackendApi/Program.cs
@@ -1,3 +1,6 @@
+using DotNet8.BankingManagementSystem.Database.EfAppDbContextModels;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDbContext>(opt =>
+{
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("DbConnection"));
+}, ServiceLifetime.Transient, ServiceLifetime.Transient);
 
 var app = builder.Build();
 
