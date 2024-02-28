f2dde09469df98fa094c19774f4eab27b1a6194e
ZinLinnhtet <mackk5504@gmail.com>
2024-02-28 11:16:23 +0630

    [Complete] StateController CRUD

---
+++ DotNet8.BankingManagementSystem.BackendApi/Models/MessageResponseListModel.cs
@@ -0,0 +1,15 @@
+﻿using DotNet8.BankingManagementSystem.Database.EfAppDbContextModels;
+
+namespace DotNet8.BankingManagementSystem.BackendApi.Models;
+
+public class MessageResponseListModel
+{
+    public bool IsSuccess { get; set; }
+    public string Message { get; set; }
+    public int PageNo { get; set; }
+    public int PageSize { get; set; }
+    public List<TblPlaceState> StateList { get; set; }
+
+
+
+}
