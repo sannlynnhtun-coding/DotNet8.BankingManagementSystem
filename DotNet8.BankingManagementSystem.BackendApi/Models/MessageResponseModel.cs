d0fc497dace8829dda1ee1ee1190097486301b31
ZinLinnhtet <mackk5504@gmail.com>
2024-02-28 11:16:29 +0630

    [Complete] StateController CRUD

---
+++ DotNet8.BankingManagementSystem.BackendApi/Models/MessageResponseModel.cs
@@ -0,0 +1,10 @@
+﻿using DotNet8.BankingManagementSystem.Database.EfAppDbContextModels;
+
+namespace DotNet8.BankingManagementSystem.BackendApi.Models;
+
+public class MessageResponseModel
+{
+    public bool IsSuccess { get; set; }
+    public string Message { get; set; }
+    public TblPlaceState State { get; set; }
+}
