1e6ec3a49fa7a4f5e1fb1e75985bc009761eb9de
ZinLinnhtet <mackk5504@gmail.com>
2024-02-28 11:16:34 +0630

    [Complete] StateController CRUD

---
+++ DotNet8.BankingManagementSystem.BackendApi/Models/StateRequestModel.cs
@@ -0,0 +1,9 @@
+﻿namespace DotNet8.BankingManagementSystem.BackendApi.Models
+{
+    public partial class StateRequestModel
+    {
+        public string StateCode { get; set; } = null!;
+        public string StateName { get; set; } = null!;
+
+    }
+}
