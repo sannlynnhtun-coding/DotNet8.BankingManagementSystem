9276660b5c54e11969587ae36888518915a82949
ZinLinnhtet <mackk5504@gmail.com>
2024-02-29 17:10:46 +0630

    [Complete] UserList

---
+++ DotNet8.BankingManagementSystem.BackendApi/Features/Users/UserController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DotNet8.BankingManagementSystem.BackendApi.Features.Users
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController : BaseController
+    {
+        private readonly UserServices _userService;
+
+        public UserController(UserServices userService)
+        {
+            _userService = userService;
+        }
+
+        #region GetUser
+
+        [HttpGet("{pageNo}/{pageSize}")]
+        public async Task<IActionResult> GetUserList(int pageNo, int pageSize)
+        {
+            try
+            {
+                var model = await _userService.GetUserList(pageNo, pageSize);
+                return Ok(model);
+            }
+            catch (Exception ex)
+            {
+                return InternalServerError(ex);
+            }
+        }
+
+        #endregion
+    }
+}
