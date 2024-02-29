8dd916647bb174d6cf27b6e870a9bcf6a98be845
ZinLinnhtet <mackk5504@gmail.com>
2024-02-29 17:10:50 +0630

    [Complete] UserList

---
+++ DotNet8.BankingManagementSystem.BackendApi/Features/Users/UserServices.cs
@@ -0,0 +1,42 @@
+﻿using DotNet8.BankingManagementSystem.Database.EfAppDbContextModels;
+using DotNet8.BankingManagementSystem.Mapper;
+using DotNet8.BankingManagementSystem.Models.State;
+using DotNet8.BankingManagementSystem.Models;
+using DotNet8.BankingManagementSystem.Models.Users;
+using Microsoft.EntityFrameworkCore;
+
+namespace DotNet8.BankingManagementSystem.BackendApi.Features.Users
+{
+    public class UserServices
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public UserServices(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public async Task<UserListResponseModel> GetUserList(int pageNo, int pageSize)
+        {
+            var query = _appDbContext.TblUsers.AsNoTracking();
+            var result = await query
+                .OrderByDescending(x => x.UserId)
+                .Skip((pageNo - 1) * pageSize)
+                .Take(pageSize)
+                .ToListAsync();
+
+            var count = await query.CountAsync();
+            int pageCount = count / pageSize;
+            if (count % pageSize > 0) pageCount++;
+            var lst = result.Select(x => x.Change()).ToList();
+
+            UserListResponseModel model = new UserListResponseModel()
+            {
+                Data = lst,
+                PageSetting = new PageSettingModel(pageNo, pageSize, pageCount),
+                Response = new MessageResponseModel(true, "Success")
+            };
+            return model;
+        }
+    }
+}
