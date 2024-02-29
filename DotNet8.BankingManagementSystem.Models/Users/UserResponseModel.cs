b88831d7a5753991cdbb9397c52e50bdac82395d
ZinLinnhtet <mackk5504@gmail.com>
2024-02-29 17:11:24 +0630

    [Complete] UserList

---
+++ DotNet8.BankingManagementSystem.Models/Users/UserResponseModel.cs
@@ -0,0 +1,7 @@
+﻿namespace DotNet8.BankingManagementSystem.Models.Users;
+
+public class UserResponseModel
+{
+    public MessageResponseModel Response { get; set; }
+    public UserModel Data { get; set; }
+}
