c1a9e4a827427c9b2f8822b7012f71c252462c26
ZinLinnhtet <mackk5504@gmail.com>
2024-02-29 17:09:38 +0630

    [Complete] UserList

---
+++ DotNet8.BankingManagementSystem.App.Api/IUserApi.cs
@@ -0,0 +1,12 @@
+﻿using DotNet8.BankingManagementSystem.Models.Users;
+using Refit;
+
+
+namespace DotNet8.BankingManagementSystem.App.Api
+{
+    public interface IUserApi
+    {
+        [Get("/api/state/{pageNo}/{pageSize}")]
+        Task<UserListResponseModel> GetStates(int pageNo, int pageSize);
+    }
+}
