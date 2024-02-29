955859ab631a7a84ae2a33493ca1acbc5a5a3330
ZinLinnhtet <mackk5504@gmail.com>
2024-02-29 17:10:03 +0630

    [Complete] UserList

---
+++ DotNet8.BankingManagementSystem.App/Pages/GeneralSetup/Users/P_User.razor.cs
@@ -0,0 +1,41 @@
+﻿using DotNet8.BankingManagementSystem.App.Api;
+using DotNet8.BankingManagementSystem.Models.State;
+using DotNet8.BankingManagementSystem.Models;
+using DotNet8.BankingManagementSystem.Models.Users;
+
+namespace DotNet8.BankingManagementSystem.App.Pages.GeneralSetup.Users
+{
+    public partial class P_User
+    {
+        private PageSettingModel _setting = new PageSettingModel()
+        {
+            PageNo = 1,
+            PageSize = 10
+        };
+
+        private UserListResponseModel? _model;
+        protected async override Task OnAfterRenderAsync(bool firstRender)
+        {
+            if (firstRender)
+            {
+                await List(_setting.PageNo, _setting.PageSize);
+            }
+        }
+        private async Task List(int pageNo, int pageSize)
+        {
+            _model = await UserApi.GetStates(pageNo, pageSize);
+            if (_model.Response.IsError)
+            {
+                //
+                return;
+            }
+            StateHasChanged();
+        }
+        private async Task PageChanged(int i)
+        {
+            _setting.PageNo = i;
+            await List(_setting.PageNo, _setting.PageSize);
+        }
+
+    }
+}
