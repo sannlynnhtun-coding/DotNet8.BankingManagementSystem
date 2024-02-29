b83bdb949ecb8a913963b928324eb419ff75f86e
ZinLinnhtet <mackk5504@gmail.com>
2024-02-29 17:11:19 +0630

    [Complete] UserList

---
+++ DotNet8.BankingManagementSystem.Models/Users/UserRequestModel.cs
@@ -0,0 +1,12 @@
+﻿namespace DotNet8.BankingManagementSystem.Models.Users;
+
+public class UserRequestModel
+{
+    public string UserName { get; set; } = null!;
+    public string FullName { get; set; } = null!;
+    public string MobileNo { get; set; } = null!;
+    public string Email { get; set; } = null!;
+    public string Nrc { get; set; } = null!;
+    public string Address { get; set; } = null!;
+
+}
