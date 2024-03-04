05458036e2b46e8dfac55c5cf35155e914659c73
minthukha-coding <minthukha1239788@gmail.com>
2024-03-04 16:09:51 +0630

    [ Complete ] TownshipCreate

---
+++ DotNet8.BankingManagementSystem.App/Pages/GeneralSetup/Township/P_TownshipCreate.razor.cs
@@ -0,0 +1,30 @@
+﻿using DotNet8.BankingManagementSystem.App.Api;
+using DotNet8.BankingManagementSystem.Models;
+using DotNet8.BankingManagementSystem.Models.State;
+using DotNet8.BankingManagementSystem.Models.TownShip;
+using DotNet8.BankingManagementSystem.Models.Users;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+
+namespace DotNet8.BankingManagementSystem.App.Pages.GeneralSetup.Township
+{
+    public partial class P_TownshipCreate
+    {
+        private TownshipRequestModel _model = new TownshipRequestModel();
+
+        private async Task OnValidSubmit(EditContext context)
+        {
+            try
+            {
+                var response = await TownshipApi.CreateTownship(_model);
+                StateHasChanged();
+            }
+            catch (Exception ex)
+            {
+                //Error
+                return;
+            }
+            NavigationManager.NavigateTo("/general-setup/township");
+        }
+    }
+}
