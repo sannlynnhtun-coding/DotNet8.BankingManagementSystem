5649afeeff7e201e5a561388868fd6b3f54e6c6c
Zin Linn Htet <mackk5504@gmail.com>
2024-03-08 16:34:48 +0630

    [InProgress] User Edit

--- DotNet8.BankingManagementSystem.App/Pages/GeneralSetup/User/P_UserRegister.razor.cs
+++ DotNet8.BankingManagementSystem.App/Pages/GeneralSetup/User/P_UserRegister.razor.cs
@@ -1,23 +1,14 @@
 ﻿using DotNet8.BankingManagementSystem.Models.State;
 using DotNet8.BankingManagementSystem.Models.Users;
-using DotNet8.BankingManagementSystem.Models;
 using DotNet8.BankingManagementSystem.Models.TownShip;
-using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.AspNetCore.Components;
 
-namespace DotNet8.BankingManagementSystem.App.Pages.GeneralSetup.User
-{
+namespace DotNet8.BankingManagementSystem.App.Pages.GeneralSetup.User;
     public partial class P_UserRegister
     {
-        private UserRequestModel _model = new UserRequestModel();
-        private UserResponseModel? _item;
+        private UserRequestModel _model = new();
         private StateListResponseModel? _stateListResponseModel;
         private TownshipListResponceModel? _townshipListResponceModel;
-
-        private PageSettingModel _setting = new PageSettingModel()
-        {
-            PageNo = 1,
-            PageSize = 10
-        };
 
         protected override async Task OnAfterRenderAsync(bool firstRender)
         {
@@ -28,24 +19,18 @@ namespace DotNet8.BankingManagementSystem.App.Pages.GeneralSetup.User
             }
         }
 
-        private async Task OnValidSubmit(EditContext context)
+        private async Task OnValidSubmit()
         {
             try
             {
-                await Add(_model);
+                var response = await UserApi.CreateUser(_model);
+                await InjectService.Go("/general-setup/user");
+                await InjectService.SuccessMessage("Registration Successful.");
             }
             catch (Exception ex)
             {
-                //Error
-                return;
+                Console.WriteLine(ex.ToString());
             }
-        }
-
-        public async Task Add(UserRequestModel requestModel)
-        {
-            _item = await UserApi.CreateUser(requestModel);
-
-            StateHasChanged();
         }
 
         private async Task ChangeState(string stateCode)
@@ -54,29 +39,4 @@ namespace DotNet8.BankingManagementSystem.App.Pages.GeneralSetup.User
             _townshipListResponceModel = await TownshipApi.GetTownShipByStateCode(_model.StateCode);
             StateHasChanged();
         }
-
-        //[JSInvokable]
-        //public void CallbackMethodSuccess(string param1, int param2)
-        //{
-        //    // This block run when click button in notiflix toast or dialog
-        //}
-        //public void CallbackMethodNo(string param1, int param2)
-        //{
-        //    NotificationService.ToastWarningAsync("[message]");
-        //    NotificationService.ToastWarningAsync("[message]", this, nameof(CallbackMethodSuccess), "param 1 value", 2222);
-
-        //}
-        //[JSInvokable]
-        //public void CallbackMethodNo()
-        //{
-        //    NotificationService.ShowLoadingAsync("Please wait a moment!");
-        //    Task.Run(async () =>
-        //    {
-        //        await Task.Delay(3000);
-        //        await NotificationService.HideLoadingAsync();
-        //        StateHasChanged();
-        //    });
-
-        //}
     }
-}
