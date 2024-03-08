57664f92e6a47b877ed6dca1960434aa9525115b
Zin Linn Htet <mackk5504@gmail.com>
2024-03-08 16:34:52 +0630

    [InProgress] User Edit

--- DotNet8.BankingManagementSystem.Models/Users/UserRequestModel.cs
+++ DotNet8.BankingManagementSystem.Models/Users/UserRequestModel.cs
@@ -2,7 +2,7 @@
 
 public class UserRequestModel
 {
-    public string UserCode { get; set; } = null!;
+    public string? UserCode { get; set; }
     public string UserName { get; set; } = null!;
     public string FullName { get; set; } = null!;
     public string Password { get; set; } = null!;
