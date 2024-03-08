0cc94544f47235782786cc9107994cf567bdc69c
Zin Linn Htet <mackk5504@gmail.com>
2024-03-08 16:34:24 +0630

    [InProgress] User Edit

--- DotNet8.BankingManagementSystem.App/Pages/GeneralSetup/Township/P_TownshipCreate.razor.cs
+++ DotNet8.BankingManagementSystem.App/Pages/GeneralSetup/Township/P_TownshipCreate.razor.cs
@@ -10,7 +10,7 @@ namespace DotNet8.BankingManagementSystem.App.Pages.GeneralSetup.Township
 {
     public partial class P_TownshipCreate
     {
-        private TownshipRequestModel _model = new TownshipRequestModel();
+        private TownshipRequestModel _model = new();
         private async Task OnValidSubmit(EditContext context)
         {
             try
