2df8de82ef43f2d284c40d548bb3ccdd443bafaf
Zin Linn Htet <mackk5504@gmail.com>
2024-03-10 22:47:27 +0630

    [Add] Confirm message

--- DotNet8.BankingManagementSystem.App/Pages/GeneralSetup/State/P_State.razor.cs
+++ DotNet8.BankingManagementSystem.App/Pages/GeneralSetup/State/P_State.razor.cs
@@ -1,47 +1,58 @@
 ﻿using DotNet8.BankingManagementSystem.Models;
 using DotNet8.BankingManagementSystem.Models.State;
 using Microsoft.AspNetCore.Components;
+using Refit;
 
-namespace DotNet8.BankingManagementSystem.App.Pages.GeneralSetup.State
+namespace DotNet8.BankingManagementSystem.App.Pages.GeneralSetup.State;
+
+public partial class P_State : ComponentBase
 {
-    public partial class P_State : ComponentBase
+    private PageSettingModel _setting = new()
     {
-        private PageSettingModel _setting = new()
+        PageNo = 1,
+        PageSize = 10
+    };
+
+    private StateListResponseModel? _model;
+
+    //[Inject]
+    //public InjectService InjectService { get; set; }
+
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        if (firstRender)
         {
-            PageNo = 1,
-            PageSize = 10
-        };
-
-        private StateListResponseModel? _model;
-
-        //[Inject]
-        //public InjectService InjectService { get; set; }
-
-        protected override async Task OnAfterRenderAsync(bool firstRender)
-        {
-            if (firstRender)
-            {
-                await List(_setting.PageNo, _setting.PageSize);
-            }
-        }
-
-        private async Task List(int pageNo, int pageSize)
-        {
-            await InjectService.EnableLoading();
-            _model = await StateApi.GetStates(pageNo, pageSize);
-            if (_model.Response.IsError)
-            {
-                //
-                return;
-            }
-            StateHasChanged();
-            await InjectService.DisableLoading();
-        }
-
-        private async Task PageChanged(int i)
-        {
-            _setting.PageNo = i;
             await List(_setting.PageNo, _setting.PageSize);
         }
+    }
+
+    private async Task List(int pageNo, int pageSize)
+    {
+        await InjectService.EnableLoading();
+        _model = await StateApi.GetStates(pageNo, pageSize);
+        if (_model.Response.IsError)
+        {
+            //
+            return;
+        }
+
+        StateHasChanged();
+        await InjectService.DisableLoading();
+    }
+
+    private async Task PageChanged(int i)
+    {
+        _setting.PageNo = i;
+        await List(_setting.PageNo, _setting.PageSize);
+    }
+
+    private async Task IsConfirmed()
+    {
+        await InjectService.IsConfirmed();
+    }
+
+    private async Task<StateResponseModel> Delete(string stateCode)
+    {
+        return await StateApi.DeleteState(stateCode);
     }
 }
