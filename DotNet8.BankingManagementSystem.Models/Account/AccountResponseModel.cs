3d0e4e513a2c8e98af7d4893188ccd5eb532190e
Zin Linn Htet <mackk5504@gmail.com>
2024-03-12 11:48:16 +0630

    [Complete] Account CRUD

---
+++ DotNet8.BankingManagementSystem.Models/Account/AccountResponseModel.cs
@@ -0,0 +1,7 @@
+﻿namespace DotNet8.BankingManagementSystem.Models.Account;
+
+public class AccountResponseModel
+{
+    public MessageResponseModel Response { get; set; }
+    public AccountModel Data { get; set; }
+}
