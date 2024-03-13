227d69a03ec9813e8861fec66333f35c122c0fe2
Zin Linn Htet <mackk5504@gmail.com>
2024-03-13 09:42:08 +0630

    [Complete] Transfer Logic

---
+++ DotNet8.BankingManagementSystem.Models/Transfer/TransferResponseModel.cs
@@ -0,0 +1,7 @@
+﻿namespace DotNet8.BankingManagementSystem.Models.Transfer;
+
+public class TransferResponseModel
+{
+    public TransferModel Data { get; set; }
+    public MessageResponseModel Response { get; set; }
+}
