6b166e29c8465c444125a935fe95a3628ab18454
Zin Linn Htet <mackk5504@gmail.com>
2024-03-13 11:50:29 +0630

    -

---
+++ DotNet8.BankingManagementSystem.Database/EfAppDbContextModels/TblAccount.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DotNet8.BankingManagementSystem.Database.EfAppDbContextModels;
+
+public partial class TblAccount
+{
+    public int AccountId { get; set; }
+
+    public string? AccountNo { get; set; }
+
+    public string CustomerCode { get; set; } = null!;
+
+    public decimal Balance { get; set; }
+}
---
+++ DotNet8.BankingManagementSystem.Database/EfAppDbContextModels/TblTransactionHistory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DotNet8.BankingManagementSystem.Database.EfAppDbContextModels;
+
+public partial class TblTransactionHistory
+{
+    public int TransactionHistoryId { get; set; }
+
+    public string FromAccountNo { get; set; } = null!;
+
+    public string ToAccountNo { get; set; } = null!;
+
+    public DateTime TransactionDate { get; set; }
+
+    public decimal Amount { get; set; }
+
+    public string AdminUserCode { get; set; } = null!;
+
+    public string TransactionType { get; set; } = null!;
+}
