b21bc24fbc9661aebd9b55b317f4ff2b8651b2ad
Zin Linn Htet <mackk5504@gmail.com>
2024-03-13 09:42:05 +0630

    [Complete] Transfer Logic

---
+++ DotNet8.BankingManagementSystem.Models/Transfer/TransferModel.cs
@@ -0,0 +1,8 @@
+﻿namespace DotNet8.BankingManagementSystem.Models.Transfer;
+
+public class TransferModel
+{
+    public string FromAccountNo { get; set; }
+    public string ToAccountNo { get; set; }
+    public decimal Amount { get; set; }
+}
