59024c97ff107d58b84d20fad89269823200759f
Zin Linn Htet <mackk5504@gmail.com>
2024-03-14 16:31:19 +0630

    [Complete] Transaction req model

---
+++ DotNet8.BankingManagementSystem.Models/Account/TransactionRequestModel.cs
@@ -0,0 +1,7 @@
+﻿namespace DotNet8.BankingManagementSystem.Models.Account;
+
+public class TransactionRequestModel
+{
+    public string AccountNo { get; set; }
+    public decimal Amount { get; set; }
+}
