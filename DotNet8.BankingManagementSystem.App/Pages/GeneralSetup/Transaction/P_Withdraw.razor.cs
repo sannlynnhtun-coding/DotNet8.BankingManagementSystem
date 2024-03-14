494dac3acc7662cea1d78941a67fb75f653bdec3
Zin Linn Htet <mackk5504@gmail.com>
2024-03-14 16:30:30 +0630

    [Complete] Transfer Page

--- DotNet8.BankingManagementSystem.App/Pages/GeneralSetup/Transaction/P_Withdraw.razor.cs
+++ DotNet8.BankingManagementSystem.App/Pages/GeneralSetup/Transaction/P_Withdraw.razor.cs
@@ -1,6 +1,23 @@
-﻿namespace DotNet8.BankingManagementSystem.App.Pages.GeneralSetup.Transaction
+﻿using DotNet8.BankingManagementSystem.Models.Account;
+using Microsoft.AspNetCore.Components.Forms;
+
+namespace DotNet8.BankingManagementSystem.App.Pages.GeneralSetup.Transaction
 {
     public partial class P_Withdraw
     {
+        private TransactionRequestModel _model = new();
+        private async Task OnValidSubmit(EditContext context)
+        {
+            try
+            {
+                var response = await TransactionApi.Withdraw(_model);
+                // await InjectService.Go("/general-setup/tran");
+                await InjectService.SuccessMessage("Withdraw Successful.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
     }
 }
