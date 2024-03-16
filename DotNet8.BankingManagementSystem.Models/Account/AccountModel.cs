16e759a6e593a552b251bc532633a7f786da58dc
Zin Linn Htet <mackk5504@gmail.com>
2024-03-16 20:03:47 +0630

    [Complete] Updating account list

--- DotNet8.BankingManagementSystem.Models/Account/AccountModel.cs
+++ DotNet8.BankingManagementSystem.Models/Account/AccountModel.cs
@@ -7,6 +7,7 @@ public class AccountModel
     public string? AccountNo { get; set; }
 
     public string CustomerCode { get; set; } = null!;
+    public string? CustomerName { get; set; }
 
     public decimal Balance { get; set; }
 }
