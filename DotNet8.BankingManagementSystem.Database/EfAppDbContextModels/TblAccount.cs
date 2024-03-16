09708af766ddaf43d562a90eb3924c3693faf638
Zin Linn Htet <mackk5504@gmail.com>
2024-03-16 20:04:21 +0630

    [Complete] Updating account list

--- DotNet8.BankingManagementSystem.Database/EfAppDbContextModels/TblAccount.cs
+++ DotNet8.BankingManagementSystem.Database/EfAppDbContextModels/TblAccount.cs
@@ -10,6 +10,7 @@ public partial class TblAccount
     public string? AccountNo { get; set; }
 
     public string CustomerCode { get; set; } = null!;
-
+    
+    public string? CustomerName { get; set; }
     public decimal Balance { get; set; }
 }
