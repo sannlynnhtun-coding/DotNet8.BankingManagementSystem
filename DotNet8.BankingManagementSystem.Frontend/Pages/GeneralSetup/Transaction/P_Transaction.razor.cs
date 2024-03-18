b5c2db8f2cc4c73cb879d60a28839d2752aef3dc
Zin Linn Htet <mackk5504@gmail.com>
2024-03-18 10:15:49 +0630

    -

--- DotNet8.BankingManagementSystem.Frontend/Pages/GeneralSetup/Transaction/P_Transaction.razor.cs
+++ DotNet8.BankingManagementSystem.Frontend/Pages/GeneralSetup/Transaction/P_Transaction.razor.cs
@@ -6,40 +6,5 @@ namespace DotNet8.BankingManagementSystem.Frontend.Pages.GeneralSetup.Transactio
 
 public partial class P_Transaction
 {
-    private PageSettingModel _setting = new()
-    {
-        PageNo = 1,
-        PageSize = 10
-    };
-
-    private TransactionHistoryListResponseModel? _model;
-
-    protected override async Task OnAfterRenderAsync(bool firstRender)
-    {
-        if (firstRender)
-        {
-            await List(_setting.PageNo, _setting.PageSize);
-            StateHasChanged();
-        }
-    }
-
-    private async Task List(int pageNo, int pageSize)
-    {
-        await InjectService.EnableLoading();
-        _model = await TransactionAPI.TransactionHistory(pageNo, pageSize);
-        if (_model.Response.IsError)
-        {
-            //
-            return;
-        }
-
-        StateHasChanged();
-        await InjectService.DisableLoading();
-    }
-
-    private async Task PageChanged(int i)
-    {
-        _setting.PageNo = i;
-        await List(_setting.PageNo, _setting.PageSize);
-    }
+   
 }
