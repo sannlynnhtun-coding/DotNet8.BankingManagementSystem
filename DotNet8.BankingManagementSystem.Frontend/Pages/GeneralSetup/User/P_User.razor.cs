feb2d58e2d69526438cba0c45b6128a3a5c1d884
Zin Linn Htet <mackk5504@gmail.com>
2024-03-18 10:16:04 +0630

    -

--- DotNet8.BankingManagementSystem.Frontend/Pages/GeneralSetup/User/P_User.razor.cs
+++ DotNet8.BankingManagementSystem.Frontend/Pages/GeneralSetup/User/P_User.razor.cs
@@ -1,72 +1,12 @@
-﻿using DotNet8.BankingManagementSystem.Models;
-using DotNet8.BankingManagementSystem.Models.Users;
-using Microsoft.AspNetCore.Components;
-using Microsoft.JSInterop;
-using MudBlazor;
-
-namespace DotNet8.BankingManagementSystem.Frontend.Pages.GeneralSetup.User;
-
-public partial class P_User : ComponentBase
-{
-    private PageSettingModel _setting = new()
-    {
-        PageNo = 1,
-        PageSize = 10
-    };
-
-    private UserListResponseModel? _model;
-
-    protected override async Task OnAfterRenderAsync(bool firstRender)
-    {
-        if (firstRender)
-        {
-            //CallbackMethodNo();
-            await List(_setting.PageNo, _setting.PageSize);
-        }
-    }
-
-    private async Task List(int pageNo, int pageSize)
-    {
-        await InjectService.EnableLoading();
-        _model = await UserApi.GetStates(pageNo, pageSize);
-        if (_model.Response.IsError)
-        {
-            //
-            return;
-        }
-
-        StateHasChanged();
-        await InjectService.DisableLoading();
-    }
-
-    private async Task PageChanged(int i)
-    {
-        _setting.PageNo = i;
-        await List(_setting.PageNo, _setting.PageSize);
-        //Nav.NavigateTo("/general-setup/user");
-    }
-
-    private async Task Delete(string UserCode)
-    {
-        var parameters = new DialogParameters<Dialog>();
-        parameters.Add(x => x.ContentText,
-            "Are you sure want to delete?");
-
-        var options = new DialogOptions() { CloseButton = true, MaxWidth = MaxWidth.ExtraSmall };
-
-        var dialog = await DialogService.ShowAsync<Dialog>("Confirm", parameters, options);
-        var result = await dialog.Result;
-        if (result.Canceled) return;
-
-        var response = await UserApi.DeleteUser(UserCode);
-        if (result is not null)
-        {
-            await InjectService.EnableLoading();
-            await List(_setting.PageNo, _setting.PageSize);
-            await InjectService.DisableLoading();
-            await InjectService.SuccessMessage("Deleting Successful.");
-        }
-
-        StateHasChanged();
-    }
-}
+﻿// using DotNet8.BankingManagementSystem.Models;
+// using DotNet8.BankingManagementSystem.Models.Users;
+// using Microsoft.AspNetCore.Components;
+// using Microsoft.JSInterop;
+// using MudBlazor;
+//
+// namespace DotNet8.BankingManagementSystem.Frontend.Pages.GeneralSetup.User;
+//
+// public partial class P_User : ComponentBase
+// {
+//    
+// }
