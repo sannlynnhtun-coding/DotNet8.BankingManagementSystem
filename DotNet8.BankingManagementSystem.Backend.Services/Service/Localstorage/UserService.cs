4f18e93a4d568576491a8d1a5090b0f20e7fd69f
Zin Linn Htet <mackk5504@gmail.com>
2024-03-20 14:30:39 +0630

    [Add] Local Storage Service

---
+++ DotNet8.BankingManagementSystem.Backend.Services/Service/Localstorage/UserService.cs
@@ -0,0 +1,21 @@
+﻿using Blazored.LocalStorage;
+
+namespace DotNet8.BankingManagementSystem.Backend.Services.Service.Localstorage;
+
+public class UserService
+{
+    private readonly ILocalStorageService _localStorageService;
+
+    public UserService(ILocalStorageService localStorageService)
+    {
+        _localStorageService = localStorageService;
+    }
+
+    public async Task CreateUser(UserModel requestModel)
+    {
+        var lst = await _localStorageService.GetItemAsync<List<UserModel>>("Tbl_User");
+        lst ??= new();
+        lst.Add(requestModel);
+        await _localStorageService.SetItemAsync("Tbl_User", lst);
+    }
+}
