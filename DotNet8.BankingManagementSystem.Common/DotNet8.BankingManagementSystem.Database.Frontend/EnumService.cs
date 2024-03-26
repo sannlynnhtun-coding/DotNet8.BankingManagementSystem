e608e298809c374b2ef8a4d70226d288a5ffdffc
Zin Linn Htet <mackk5504@gmail.com>
2024-03-26 16:45:33 +0630

    [Add] Enum Service

---
+++ DotNet8.BankingManagementSystem.Common/DotNet8.BankingManagementSystem.Database.Frontend/EnumService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace DotNet8.BankingManagementSystem.Database.Frontend
+{
+    public enum EnumService
+    {
+        Tbl_User,
+        Tbl_Account,
+    }
+
+    public static class EnumServiceExtensions
+    {
+        public static string GetKeyName(this EnumService key)
+        {
+            switch (key)
+            {
+                case EnumService.Tbl_User:
+                    return "Tbl_User";
+                case EnumService.Tbl_Account:
+                    return "Tbl_Account";
+                default:
+                    throw new ArgumentException("Invalid enum value", nameof(key));
+            }
+        }
+    }
+}
