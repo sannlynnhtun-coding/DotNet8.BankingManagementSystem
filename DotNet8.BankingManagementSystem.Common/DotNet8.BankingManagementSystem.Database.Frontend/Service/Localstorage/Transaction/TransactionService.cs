c247d94c20bfed7d13cc68fdb1b2731e8013f4c4
Zin Linn Htet <mackk5504@gmail.com>
2024-03-27 16:58:12 +0630

    [Complete] Transaction Service

--- DotNet8.BankingManagementSystem.Common/DotNet8.BankingManagementSystem.Database.Frontend/Service/Localstorage/Transaction/TransactionService.cs
+++ DotNet8.BankingManagementSystem.Common/DotNet8.BankingManagementSystem.Database.Frontend/Service/Localstorage/Transaction/TransactionService.cs
@@ -18,7 +18,7 @@ public class TransactionService
     public async Task<AccountResponseModel> Withdraw(AccountModel requestModel, decimal amount)
     {
         AccountResponseModel model = new AccountResponseModel();
-        var lst = await _localStorageService.GetAccountList(EnumService.Tbl_Account.GetKeyName());
+        var lst = await _localStorageService.GetList<AccountModel>(EnumService.Tbl_Account.GetKeyName());
         var result = lst.FirstOrDefault(x => x.AccountNo == requestModel.AccountNo);
 
         if (result is null)
@@ -36,7 +36,7 @@ public class TransactionService
         result.Balance -= amount;
 
         lst[lst.FindIndex(x => x.AccountNo == result.AccountNo)] = result;
-        await _localStorageService.SetAccount(lst);
+        await _localStorageService.SetList(EnumService.Tbl_Account.GetKeyName(),lst);
 
         model.Data = result;
         model.Response = new MessageResponseModel(true, "Withdrawal successful.");
@@ -47,7 +47,7 @@ public class TransactionService
     public async Task<AccountResponseModel> Deposit(AccountModel requestModel, decimal amount)
     {
         AccountResponseModel model = new AccountResponseModel();
-        var lst = await _localStorageService.GetAccountList(EnumService.Tbl_Account.GetKeyName());
+        var lst = await _localStorageService.GetList<AccountModel>(EnumService.Tbl_Account.GetKeyName());
         var result = lst.FirstOrDefault(x => x.AccountNo == requestModel.AccountNo);
 
         if (result == null)
@@ -60,7 +60,7 @@ public class TransactionService
 
         lst[lst.FindIndex(x => x.AccountNo == result.AccountNo)] = result;
 
-        await _localStorageService.SetAccount(lst);
+        await _localStorageService.SetList(EnumService.Tbl_Account.GetKeyName(),lst);
 
         model.Data = result;
         model.Response = new MessageResponseModel(true, "Deposit successful.");
@@ -72,10 +72,10 @@ public class TransactionService
     {
         TransferResponseModel model = new TransferResponseModel();
 
-        var lst = await _localStorageService.GetAccountList(EnumService.Tbl_Account.GetKeyName());
+        var lst = await _localStorageService.GetList<AccountModel>(EnumService.Tbl_Account.GetKeyName());
         var fromAccount = lst.FirstOrDefault(x => x.AccountNo == requestModel.FromAccountNo);
 
-        var lst1 = await _localStorageService.GetAccountList(EnumService.Tbl_Account.GetKeyName());
+        var lst1 = await _localStorageService.GetList<AccountModel>(EnumService.Tbl_Account.GetKeyName());
         var toAccount = lst1.FirstOrDefault(x => x.AccountNo == requestModel.ToAccountNo);
 
         if (fromAccount is null)
@@ -100,10 +100,10 @@ public class TransactionService
         toAccount.Balance += requestModel.Amount;
 
         lst[lst.FindIndex(x => x.AccountNo == fromAccount.AccountNo)] = fromAccount;
-        await _localStorageService.SetAccount(lst);
+        await _localStorageService.SetList(EnumService.Tbl_Account.GetKeyName(),lst);
 
         lst1[lst1.FindIndex(x => x.AccountNo == toAccount.AccountNo)] = toAccount;
-        await _localStorageService.SetAccount(lst1);
+        await _localStorageService.SetList(EnumService.Tbl_Account.GetKeyName(),lst1);
 
         model.Data.FromAccountNo = Convert.ToString(fromAccount);
         model.Data.ToAccountNo = Convert.ToString(toAccount);
