22b241f2b7972d1cd2fcce442b14eaad11099e3f
Zin Linn Htet <mackk5504@gmail.com>
2024-03-27 16:55:50 +0630

    [Fix] LocalStorage Service

--- DotNet8.BankingManagementSystem.Common/DotNet8.BankingManagementSystem.Database.Frontend/LocalStorageService.cs
+++ DotNet8.BankingManagementSystem.Common/DotNet8.BankingManagementSystem.Database.Frontend/LocalStorageService.cs
@@ -13,22 +13,32 @@ public class LocalStorageService
         _localStorageService = localStorageService;
     }
 
-    public async Task<List<AccountModel>> GetAccountList(string keyName)
+    // public async Task<List<AccountModel>> GetAccountList(string keyName)
+    // {
+    //     return await _localStorageService.GetItemAsync<List<AccountModel>>(keyName);
+    // }
+    //
+    // public async Task<List<UserModel>> GetUserList(string keyName)
+    // {
+    //     return await _localStorageService.GetItemAsync<List<UserModel>>(keyName);
+    // }
+
+    public async Task<List<T>> GetList<T>(string keyName)
     {
-        return await _localStorageService.GetItemAsync<List<AccountModel>>(keyName);
-    }
-    public async Task<List<UserModel>> GetUserList(string keyName)
-    {
-        return await _localStorageService.GetItemAsync<List<UserModel>>(keyName);
+        return await _localStorageService.GetItemAsync<List<T>>(keyName);
     }
 
-    public async Task SetAccount(List<AccountModel> lst)
+    public async Task SetList<T>(string keyName, List<T> lst)
     {
-        await _localStorageService.SetItemAsync(EnumService.Tbl_Account.GetKeyName(), lst);
+        await _localStorageService.SetItemAsync(keyName, lst);
     }
-    
-    public async Task SetUser(List<UserModel> lst)
-    {
-        await _localStorageService.SetItemAsync(EnumService.Tbl_Account.GetKeyName(), lst);
-    }
+    // public async Task SetAccount(List<AccountModel> lst)
+    // {
+    //     await _localStorageService.SetItemAsync(EnumService.Tbl_Account.GetKeyName(), lst);
+    // }
+    //
+    // public async Task SetUser(List<UserModel> lst)
+    // {
+    //     await _localStorageService.SetItemAsync(EnumService.Tbl_Account.GetKeyName(), lst);
+    // }
 }
