33fde3901de967c70040d000d50ea15135a90bf9
Zin Linn Htet <mackk5504@gmail.com>
2024-04-03 23:27:44 +0630

    -

--- DotNet8.BankingManagementSystem.Common/DotNet8.BankingManagementSystem.Frontend.Api/Features/Account/AccountService.cs
+++ DotNet8.BankingManagementSystem.Common/DotNet8.BankingManagementSystem.Frontend.Api/Features/Account/AccountService.cs
@@ -1,7 +1,4 @@
-﻿using DotNet8.BankingManagementSystem.Frontend.Api.Services;
-using System.Reflection;
-
-namespace DotNet8.BankingManagementSystem.Frontend.Api.Features.Account;
+﻿namespace DotNet8.BankingManagementSystem.Frontend.Api.Features.Account;
 
 public class AccountService
 {
@@ -12,6 +9,8 @@ public class AccountService
         _localStorageService = localStorageService;
     }
 
+    #region GetAccounts
+
     public async Task<AccountListResponseModel> GetAccounts()
     {
         AccountListResponseModel model = new AccountListResponseModel();
@@ -20,6 +19,37 @@ public class AccountService
         model.Response = new MessageResponseModel(true, "Success.");
         return model;
     }
+
+    #endregion
+
+    #region GetAccountList
+
+    public async Task<AccountListResponseModel> GetAccountList(int pageNo, int pageSize)
+    {
+        var query = await _localStorageService.GetList<TblAccount>(EnumService.Tbl_Account.ToString());
+        var result = query
+            .OrderByDescending(x => x.AccountNo)
+            .Skip((pageNo - 1) * pageSize)
+            .Take(pageSize)
+            .ToList();
+
+        var count = query.Count();
+        int pageCount = count / pageSize;
+        if (count % pageSize > 0) pageCount++;
+        var lst = result.Select(x => x.Change()).ToList();
+
+        AccountListResponseModel model = new AccountListResponseModel
+        {
+            Data = lst,
+            PageSetting = new PageSettingModel(pageNo, pageSize, pageCount),
+            Response = new MessageResponseModel(true, "Success")
+        };
+        return model;
+    }
+
+    #endregion
+
+    #region GetAccount
 
     public async Task<AccountResponseModel> GetAccount(string accountNo)
     {
@@ -38,25 +68,54 @@ public class AccountService
         return model;
     }
 
+    #endregion
+
+    #region CreateAccount
+
     public async Task<AccountResponseModel> CreateAccount(AccountRequestModel requestModel)
     {
         AccountResponseModel model = new AccountResponseModel();
-        var lst = await _localStorageService.GetList<AccountModel>(EnumService.Tbl_Account.GetKeyName());
-        lst ??= new List<AccountModel>();
-
-        lst.Add(new AccountModel
+        try
         {
-            AccountNo = requestModel.AccountNo,
-            CustomerName = requestModel.CustomerName,
-            Balance = requestModel.Balance,
-            CustomerCode = requestModel.CustomerCode,
-        });
+            var item = requestModel.Change();
+            var query = await _localStorageService.GetList<TblAccount>(EnumService.Tbl_Account.ToString());
+            query ??= new List<TblAccount>();
+            query.Add(item);
+            await _localStorageService.SetList(EnumService.Tbl_Account.ToString(), query);
+            model.Data = item.Change();
+            model.Response = new MessageResponseModel(true, "Account has created successfully.");
+        }
+        catch (Exception ex)
+        {
+            model.Response = new MessageResponseModel(false, ex);
+        }
 
-        await _localStorageService.SetList(EnumService.Tbl_Account.GetKeyName(), lst);
-
-        model.Response = new MessageResponseModel(true, "Account has been registered.");
         return model;
     }
+
+    #endregion
+
+    // public async Task<AccountResponseModel> CreateAccountv1(AccountRequestModel requestModel)
+    // {
+    //     AccountResponseModel model = new AccountResponseModel();
+    //     var lst = await _localStorageService.GetList<AccountModel>(EnumService.Tbl_Account.GetKeyName());
+    //     lst ??= new List<AccountModel>();
+    //
+    //     lst.Add(new AccountModel
+    //     {
+    //         AccountNo = requestModel.AccountNo,
+    //         CustomerName = requestModel.CustomerName,
+    //         Balance = requestModel.Balance,
+    //         CustomerCode = requestModel.CustomerCode,
+    //     });
+    //
+    //     await _localStorageService.SetList(EnumService.Tbl_Account.GetKeyName(), lst);
+    //
+    //     model.Response = new MessageResponseModel(true, "Account has been registered.");
+    //     return model;
+    // }
+
+    #region UpdateAccount
 
     public async Task<AccountResponseModel> UpdateAccount(string accountNo, AccountRequestModel requestModel)
     {
@@ -85,6 +144,10 @@ public class AccountService
         return model;
     }
 
+    #endregion
+
+    #region DeleteAccount
+
     public async Task<AccountResponseModel> DeleteAccount(string accountNo)
     {
         AccountResponseModel model = new AccountResponseModel();
@@ -103,4 +166,6 @@ public class AccountService
         model.Response = new MessageResponseModel(true, "Account has been removed.");
         return model;
     }
+
+    #endregion
 }
