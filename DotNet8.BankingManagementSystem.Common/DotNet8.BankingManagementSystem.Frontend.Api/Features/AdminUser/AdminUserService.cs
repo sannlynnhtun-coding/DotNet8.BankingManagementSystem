11cad07e8cf5786eeec829af4c952de788792f41
Zin Linn Htet <mackk5504@gmail.com>
2024-04-03 23:27:50 +0630

    -

---
+++ DotNet8.BankingManagementSystem.Common/DotNet8.BankingManagementSystem.Frontend.Api/Features/AdminUser/AdminUserService.cs
@@ -0,0 +1,11 @@
+﻿namespace DotNet8.BankingManagementSystem.Frontend.Api.Features.AdminUser;
+
+public class AdminUserService
+{
+    private readonly LocalStorageService _localStorageService;
+
+    public AdminUserService(LocalStorageService localStorageService)
+    {
+        _localStorageService = localStorageService;
+    }
+}
