a41b35064166e2287544491bf0a615b1dee66d41
Zin Linn Htet <mackk5504@gmail.com>
2024-04-04 00:53:04 +0630

    -

--- DotNet8.BankingManagementSystem.Common/DotNet8.BankingManagementSystem.Frontend.Api/Features/AdminUser/AdminUserService.cs
+++ DotNet8.BankingManagementSystem.Common/DotNet8.BankingManagementSystem.Frontend.Api/Features/AdminUser/AdminUserService.cs
@@ -8,4 +8,144 @@ public class AdminUserService
     {
         _localStorageService = localStorageService;
     }
+
+    #region GetAdminUsers
+
+    public async Task<AdminUserListResponseModel> GetAdminUsers()
+    {
+        AdminUserListResponseModel model = new AdminUserListResponseModel();
+        var query = await _localStorageService.GetList<TblAdminUser>(EnumService.Tbl_AdminUser.ToString());
+        var result = query
+            .OrderByDescending(x => x.AdminUserId)
+            .ToList();
+        
+        model.Data = result.Select(x => x.Change()).ToList();
+        model.Response = new MessageResponseModel(true, "Success");
+        return model;
+    }
+
+    #endregion
+
+    #region GetAdminUserList
+
+    public async Task<AdminUserListResponseModel> GetAdminUserList(int pageNo, int pageSize)
+    {
+        AdminUserListResponseModel model = new AdminUserListResponseModel();
+        var query = await _localStorageService.GetList<TblAdminUser>(EnumService.Tbl_AdminUser.ToString());
+        var result = query
+            .OrderByDescending(x => x.AdminUserId)
+            .Skip((pageNo - 1) * pageSize)
+            .Take(pageSize)
+            .ToList();
+
+        var count = query.Count();
+        int pageCount = count / pageSize;
+        if (count % pageSize > 0) pageCount++;
+
+        model.Data = result.Select(x => x.Change()).ToList();
+        model.PageSetting = new PageSettingModel(pageNo, pageSize, pageCount);
+        model.Response = new MessageResponseModel(true, "Success");
+        return model;
+    }
+
+    #endregion
+
+    #region CreateAdminUser
+
+    public async Task<AdminUserResponseModel> CreateAdminUser(AdminUserRequestModel requestModel)
+    {
+        AdminUserResponseModel model = new AdminUserResponseModel();
+        try
+        {
+            var item = requestModel.Change();
+            var query = await _localStorageService.GetList<TblAdminUser>(EnumService.Tbl_AdminUser.ToString());
+            query ??= new List<TblAdminUser>();
+            query.Add(item);
+            await _localStorageService.SetList(EnumService.Tbl_AdminUser.ToString(), query);
+            model.Data = item.Change();
+            model.Response = new MessageResponseModel(true, "Admin has created successfully.");
+        }
+        catch (Exception ex)
+        {
+            model.Response = new MessageResponseModel(false, ex);
+        }
+
+        return model;
+    }
+
+
+    #endregion
+
+    #region GetAdminUserByCode
+
+    public async Task<AdminUserResponseModel> GetAdminUserByCode(string adminUserCode)
+    {
+        AdminUserResponseModel model = new AdminUserResponseModel();
+        var lst = await _localStorageService.GetList<TblAdminUser>(EnumService.Tbl_AdminUser.ToString());
+        lst ??= new();
+        var item = lst.FirstOrDefault(x => x.AdminUserCode == adminUserCode);
+        if (item is null)
+        {
+            model.Response = new MessageResponseModel(false, "No Data Found.");
+            return model;
+        }
+
+        model.Data = item.Change();
+        model.Response = new MessageResponseModel(true, "Success.");
+        return model;
+    }
+
+    #endregion
+
+    #region UpdateAdminUser
+
+    public async Task<AdminUserResponseModel> UpdateAdminUser(AdminUserRequestModel requestModel)
+    {
+        AdminUserResponseModel model = new AdminUserResponseModel();
+        var lst = await _localStorageService.GetList<TblAdminUser>(EnumService.Tbl_AdminUser.ToString());
+        var result = lst.FirstOrDefault(x => x.AdminUserCode == requestModel.AdminUserCode);
+        var index = lst.FindIndex(x => result != null && x.AdminUserCode == result.AdminUserCode);
+        if (result is null)
+        {
+            model.Response = new MessageResponseModel(false, "No Data Found.");
+            return model;
+        }
+
+        result.AdminUserCode = requestModel.AdminUserCode;
+        result.AdminUserName = requestModel.AdminUserName;
+        result.MobileNo = requestModel.MobileNo;
+        result.UserRoleCode = requestModel.UserRoleCode;
+        lst[index] = result;
+
+        await _localStorageService.SetList(EnumService.Tbl_AdminUser.ToString(), lst);
+        model.Data = result.Change();
+        model.Response = new MessageResponseModel(true, "Admin has been removed.");
+        return model;
+    }
+
+
+    #endregion
+
+    #region DeleteAdminUser
+
+    public async Task<AdminUserResponseModel> DeleteAdminUser(string adminUserCode)
+    {
+        AdminUserResponseModel model = new AdminUserResponseModel();
+        var lst = await _localStorageService.GetList<TblAdminUser>(EnumService.Tbl_AdminUser.ToString());
+        lst ??= new();
+        var item = lst.FirstOrDefault(x => x.AdminUserCode == adminUserCode);
+        if (item == null)
+        {
+            model.Response = new MessageResponseModel(false, "No Data Found.");
+            return model;
+        }
+
+        lst.Remove(item);
+        await _localStorageService.SetList(EnumService.Tbl_AdminUser.ToString(), lst);
+        model.Response = new MessageResponseModel(true, "Account has been removed.");
+        return model;
+    }
+
+
+    #endregion
 }
