02b6aa570b2c4d66b41b5e5b1925b10cc88288c0
Zin Linn Htet <mackk5504@gmail.com>
2024-04-03 23:28:04 +0630

    -

--- DotNet8.BankingManagementSystem.Common/DotNet8.BankingManagementSystem.Frontend.Api/GlobalUsings.cs
+++ DotNet8.BankingManagementSystem.Common/DotNet8.BankingManagementSystem.Frontend.Api/GlobalUsings.cs
@@ -1,11 +1,21 @@
 ﻿// Global using directives
 
 global using Blazored.LocalStorage;
+global using DotNet8.BankingManagementSystem.Database.EfAppDbContextModels;
+global using DotNet8.BankingManagementSystem.Frontend.Api.Services;
 global using DotNet8.BankingManagementSystem.Models;
 global using DotNet8.BankingManagementSystem.Models.Account;
 global using DotNet8.BankingManagementSystem.Models.Transfer;
 global using DotNet8.BankingManagementSystem.Models.Users;
 global using DotNet8.BankingManagementSystem.Mapper;
+global using DotNet8.BankingManagementSystem.Models.State;
 global using DotNet8.BankingManagementSystem.Database.EfAppDbContextModels;
+global using DotNet8.BankingManagementSystem.Frontend.Api.Features.Account;
+global using DotNet8.BankingManagementSystem.Frontend.Api.Features.State;
+global using DotNet8.BankingManagementSystem.Frontend.Api.Features.Township;
+global using DotNet8.BankingManagementSystem.Frontend.Api.Features.Transaction;
+global using DotNet8.BankingManagementSystem.Frontend.Api.Features.User;
 global using DotNet8.BankingManagementSystem.Frontend.Api.Services;
 global using DotNet8.BankingManagementSystem.Models.State;
+global using DotNet8.BankingManagementSystem.Models.TownShip;
+global using DotNet8.BankingManagementSystem.Models.TransactionHistory;
