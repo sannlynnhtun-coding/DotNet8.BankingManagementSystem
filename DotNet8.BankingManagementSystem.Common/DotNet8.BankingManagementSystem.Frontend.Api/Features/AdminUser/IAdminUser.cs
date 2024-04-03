b9f4e72c73f8f418226ddbace2737b23a2896d54
Zin Linn Htet <mackk5504@gmail.com>
2024-04-04 00:54:34 +0630

    [Complete] Admin Api Interface

--- DotNet8.BankingManagementSystem.Common/DotNet8.BankingManagementSystem.Frontend.Api/Features/AdminUser/IAdminUser.cs
+++ DotNet8.BankingManagementSystem.Common/DotNet8.BankingManagementSystem.Frontend.Api/Features/AdminUser/IAdminUser.cs
@@ -1,25 +1,22 @@
-﻿using DotNet8.BankingManagementSystem.Models.AdminUser;
-using Refit;
-
-namespace DotNet8.BankingManagementSystem.Frontend.Api.Features.AdminUser;
+﻿namespace DotNet8.BankingManagementSystem.Frontend.Api.Features.AdminUser;
 
 public interface IAdminUser
 {
     [Get("/api/AdminUser")]
-    Task<AdminUserListResponseModel> GetAdminUser();
-
-    [Get("/api/AdminUser/{AdminUserCode}")]
-    Task<AdminUserResponseModel> GetAdminUser(string AdminUserCode);
+    Task<AdminUserListResponseModel> GetAdminUsers();
 
     [Get("/api/AdminUser/{pageNo}/{pageSize}")]
     Task<AdminUserListResponseModel> GetAdminUserList(int pageNo, int pageSize);
 
+    [Get("/api/AdminUser/{AdminUserCode}")]
+    Task<AdminUserResponseModel> GetAdminUserByCode(string adminUserCode);
+
     [Post("/api/AdminUser")]
     Task<AdminUserResponseModel> CreateAdminUser(AdminUserRequestModel requestModel);
 
-    [Delete("/api/AdminUser/{AdminUserCode}")]
-    Task<AdminUserResponseModel> DeleteAdminUser(string AdminUserCode);
-
     [Put("/api/AdminUser/{AdminUserCode}")]
-    Task<AdminUserResponseModel> UpdateAdminUser(string AdminUserCode, AdminUserRequestModel requestModel);
+    Task<AdminUserResponseModel> UpdateAdminUser(AdminUserRequestModel requestModel);
+
+    [Delete("/api/AdminUser/{AdminUserCode}")]
+    Task<AdminUserResponseModel> DeleteAdminUser(string adminUserCode);
 }
