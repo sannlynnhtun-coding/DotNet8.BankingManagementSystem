da1d26fc9d090a5d704a321fc8b908ac5f1db042
mgchit <chitlunh@gmail.com>
2024-04-04 23:37:16 +0630

    [Fixde] error

--- DotNet8.BankingManagementSystem.Common/DotNet8.BankingManagementSystem.Frontend.Api/Features/User/IUserApi.cs
+++ DotNet8.BankingManagementSystem.Common/DotNet8.BankingManagementSystem.Frontend.Api/Features/User/IUserApi.cs
@@ -2,18 +2,18 @@
 
 public interface IUserApi
 {
-    [Get("/api/user/{pageNo}/{pageSize}")]
+    [Get("/api/user/list/{pageNo}/{pageSize}")]
     Task<UserListResponseModel> GetUserList(int pageNo, int pageSize);
 
     [Get("/api/user/{userCode}")]
     Task<UserResponseModel> GetUserByCode(string userCode);
 
-    [Post("/api/user")]
+    [Post("/api/user/create")]
     Task<UserResponseModel> CreateUser(UserRequestModel requestModel); 
     
-    [Put("/api/user/{userCode}")]
+    [Put("/api/user/update")]
     Task<UserResponseModel> UpdateUser(UserRequestModel requestModel);
 
-    [Delete("/api/user/{userCode}")]
+    [Delete("/api/user/delete{userCode}")]
     Task<UserResponseModel> DeleteUser(string userCode);
 }
