e79e11c61529dd9690fc252d560192d161f35773
Zin Linn Htet <mackk5504@gmail.com>
2024-04-09 11:13:14 +0630

    [Complete] Transfer Method

--- DotNet8.BankingManagementSystem.Frontend/Pages/Transaction/Transfer/P_Transfer.razor.cs
+++ DotNet8.BankingManagementSystem.Frontend/Pages/Transaction/Transfer/P_Transfer.razor.cs
@@ -1,14 +1,27 @@
-﻿namespace DotNet8.BankingManagementSystem.Frontend.Pages.Transaction.Transfer;
+﻿using DotNet8.BankingManagementSystem.Models.Users;
+
+namespace DotNet8.BankingManagementSystem.Frontend.Pages.Transaction.Transfer;
 
 public partial class P_Transfer : ComponentBase
 {
-    private TransferModel _model = new();
+    private TransferModel reqModel = new();
+    private AccountRequestModel _model = new();
+    private AccountListResponseModel _accountListResponseModel = new AccountListResponseModel();
+    private UserListResponseModel _userListResponseModel = new UserListResponseModel();
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        if (firstRender)
+        {
+            _accountListResponseModel = await ApiService.GetAccountList(0);
+            _userListResponseModel = await ApiService.GetUserList(0);
+        }
+    }
 
     private async Task OnValidSubmit(EditContext context)
     {
         try
         {
-            var response = await ApiService.Transfer(_model);
+            var response = await ApiService.Transfer(reqModel);
             await InjectService.SuccessMessage("Balance transfer Successful.");
             await InjectService.Go("/report/transaction-history");
         }
